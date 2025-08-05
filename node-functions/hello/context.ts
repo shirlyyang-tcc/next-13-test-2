d7ac56b37f2db3b88fb74bb70598189c7e5a7293
shirlyyang <shirlyyang@tencent.com>
2025-08-05 19:35:14 +0800

    add uuid

--- node-functions/hello/context.ts
+++ node-functions/hello/context.ts
@@ -1,8 +1,8 @@
 export const onRequest = (context) => {
   // console.log("Context from node-functions!", context);
-  const { params, server, clientIp, geo } = context;
+  const { params, server, clientIp, geo, uuid } = context;
   return new Response(
     "Context from node-functions!" +
-      JSON.stringify({ params, server, clientIp, geo, headers: context.request.headers })
+      JSON.stringify({ params, server, clientIp, geo, headers: context.request.headers, uuid })
   );
 };
