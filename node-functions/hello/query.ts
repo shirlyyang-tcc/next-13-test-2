b3ed6470c74bc427d68ecf1503c57bc46b682d7f
shirlyyang <shirlyyang@tencent.com>
2025-08-20 15:36:38 +0800

    add function

---
+++ node-functions/hello/query.ts
@@ -0,0 +1,9 @@
+export async function onRequest(context: { request: { method: string; headers: Record<string, string>; body: unknown } }) {
+  const { request } = context;
+  const query = request.query;
+  const cookies = request.cookies;
+  console.log("cookies:", cookies);
+  return new Response(JSON.stringify({ message: 'query:' + JSON.stringify(query) + ':cookies:' + JSON.stringify(cookies) }), {
+    status: 200,
+  });
+}
