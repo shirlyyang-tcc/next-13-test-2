779ca6029280b0aad281555c3153777ce253a5cc
shirlyyang <shirlyyang@tencent.com>
2025-08-20 16:30:09 +0800

    update

--- node-functions/hello/context.ts
+++ node-functions/hello/context.ts
@@ -1,8 +1,6 @@
 export const onRequest = async (context) => {
   // console.log("Context from node-functions!", context);
   const { request, params, server, clientIp, geo, uuid, env } = context;
-  const json = await request.body.json;
-  console.log("🚀 ~ onRequest ~ body:", json);
   return new Response(
     "Context from node-functions!" +
       JSON.stringify({ params, server, clientIp, geo, headers: context.request.headers, uuid, env })
